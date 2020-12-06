331d2f0cef37a7e8bee0c96a4ea79e9fce22d738
mofflar <moffel@toffel>
2020-12-06 21:14:09 +0100

    Key combination for enabling MIDI sound

--- keyboards/preonic/keymaps/mofflar/keymap.c
+++ keyboards/preonic/keymaps/mofflar/keymap.c
@@ -90,7 +90,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * | Caps |      |      |      |      |      |      |   1  |   2  |   3  |   -  |PrScr |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Shift| RGB  |      |      |      |      |NumLck|   0  |   .  |Enter |   +  |Enter |
+ * | Shift| RGB  |MusTog|MusMod|      |      |NumLck|   0  |   .  |Enter |   +  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Ctrl | Meta | Alt  | rAlt |Lower |    Space    | Base | Next | Vol- | Vol+ | Play |
  * `-----------------------------------------------------------------------------------'
@@ -99,7 +99,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_P7,   KC_P8,   KC_P9,   KC_PSLS, KC_BSPC,
   KC_TAB,  KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_P4,   KC_P5,   KC_P6,   KC_PAST, KC_DEL,
   KC_CAPS, _______, _______, _______, _______, _______, _______, KC_P1,   KC_P2,   KC_P3,   KC_PMNS, KC_PSCR,
-  KC_LSFT, RGB_TOG, _______, _______, _______, _______, KC_NLCK, KC_P0,   KC_PDOT, KC_PENT, KC_PPLS, KC_ENT,
+  KC_LSFT, RGB_TOG, MU_TOG,  MU_MOD,  _______, _______, KC_NLCK, KC_P0,   KC_PDOT, KC_PENT, KC_PPLS, KC_ENT,
   KC_LCTL, KC_LGUI, KC_LALT, KC_ALGR, _______, KC_SPC,  KC_SPC,  _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
 ),
 
