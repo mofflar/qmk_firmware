8012c3a54fb1c2df702781ca1e1c887856845dab
mofflar <moffel@toffel>
2020-12-13 14:40:24 +0100

    New layout

--- keyboards/preonic/keymaps/mofflar/keymap.c
+++ keyboards/preonic/keymaps/mofflar/keymap.c
@@ -48,17 +48,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * | Caps |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   Ö  |  Ä   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   -  |Enter |
+ * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |  Up  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl | Meta | Alt  | rAlt |Lower |    Space    |Raise | Left | Down |  Up  |Right |
+ * | Ctrl | Meta | Alt  |  fn  |Lower |    Space    |Raise | rAlt | Left | Down |Right |
  * `-----------------------------------------------------------------------------------'
  */
 [_BASE] = LAYOUT_preonic_grid(
   KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    SE_ARNG,
   KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    SE_ODIA, SE_ADIA,
-  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  SE_MINS, KC_ENT,
-  KC_LCTL, KC_LGUI, KC_LALT, KC_ALGR, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   KC_ENT,
+  KC_LCTL, KC_LGUI, KC_LALT, KC_ALGR, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_ALGR, KC_LEFT, KC_DOWN, KC_RGHT
 ),
 
 /* Lower
@@ -69,7 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * | Caps |      |      |      |      |      |      |      |      |      |  '   |PrScr |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Shift|  <   |      |      |      |      | Home | End  | PgUp |PgDwn |Insert|Enter |
+ * | Shift|  <   |      |      |      | Ins  | Home | End  | PgUp |PgDwn |  -   |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Ctrl | Meta | Alt  | rAlt | Base |    Space    |Raise | Next | Vol- | Vol+ | Play |
  * `-----------------------------------------------------------------------------------'
@@ -78,7 +78,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,  SE_SECT, _______, _______, _______, _______, _______, _______, _______, SE_PLUS, SE_ACUT, KC_BSPC,
   KC_TAB,  _______, _______, _______, _______, _______, _______, _______, _______, _______, SE_DIAE, KC_DEL,
   KC_CAPS, _______, _______, _______, _______, _______, _______, _______, _______, _______, SE_QUOT, KC_PSCR,
-  KC_LSFT, SE_LABK, _______, _______, _______, _______, KC_HOME, KC_END,  KC_PGUP, KC_PGDN, KC_INS,  KC_ENT,
+  KC_LSFT, SE_LABK, _______, _______, _______, KC_INS,  KC_HOME, KC_END,  KC_PGUP, KC_PGDN, SE_MINS, KC_ENT,
   KC_LCTL, KC_LGUI, KC_LALT, KC_ALGR, _______, KC_SPC,  KC_SPC,  _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
 ),
 
